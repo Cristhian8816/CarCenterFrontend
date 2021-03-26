548f0742754a122ae4a39796e361fcfe559dc515
Cristhian8816 <cristhian.julian@hotmail.com>
2021-03-26 15:52:43 -0500

    building form to maintenance program

--- src/app/admin/components/form-patient/form-patient.component.ts
+++ src/app/admin/components/form-patient/form-patient.component.ts
@@ -22,23 +22,21 @@ import { IfStmt } from '@angular/compiler';
   styleUrls: ['./form-patient.component.scss']
 })
 export class FormPatientComponent implements OnInit {
-  form: FormGroup;
-  patientsNames = [];
-  patientsIds = [];
-  appointments = [];
-  apointmentsTypes = ['Neurologia', 'Medicina general', 'Pediatria', 'Odontologia' ];
-  IdAppointment;
-  PatientName;
-  appointmentDate: Date;
-  patientNameSelected;
-  appointmentTypeSelected;
-  SameAppointmentDate = [];
-  DateAppointmentPatient;
-  SameDate;
+  form: FormGroup;  
+  FullName: string;
+  IdentificationsTypes =['Cedula de Ciudadania','Cedula de Extranjeria','Tarjeta de Identidad']
+  identificationtype = [];
+  ID: string;
+  CellPhone: string;
+  Address: string;
+  Email: string;
+  Marca;
+  Description;
+  initialDate: Date;
+  
   constructor(
     private formBuilder: FormBuilder,
-    private clientsService: ClientsService,
-    private appointmentsService: AppointmentsService,
+    private clientsService: ClientsService,  
     private router: Router,
     private storage: AngularFireStorage,
   ) {
@@ -46,97 +44,40 @@ export class FormPatientComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    this.FecthAppointmentsID();
-    this.FecthPatients();
-    this.fetchAppointments();
+
    }
-
-  saveProduct(event: Event) {
+   
+   saveInformation(event: Event) {
     event.preventDefault();
-    if (this.form.valid) {
-      // Search Patient ID for Patient Name
-      // -------------------------------------------------------
-      let index = 0;
-      let selectedPatientID = 0;
-      this.patientsNames.forEach(patientName => {
-        if (patientName === this.patientNameSelected[0]) {
-          selectedPatientID = this.patientsIds[index];
-        }
-        index++;
-      });
-      // -------------------------------------------------------
-
-      // Verify if patient has appointment that Date
-      this.AppointmentSameDate(this.appointmentDate, selectedPatientID);
-      console.log(this.SameAppointmentDate);
-      if (this.SameAppointmentDate.indexOf('true') > -1) {
-        alert('Please, Change the Appointment Date for this Patient because has an Appointment for this day');
-      } else {
-        // Post to Appointments
-        const appointment = { 'AppointmentId': this.IdAppointment,
-        'Date': this.appointmentDate,
-        'AppointmentType': this.appointmentTypeSelected[0],
-        'PatientID': selectedPatientID};
-        
-        this.appointmentsService.createAppointment(appointment)
-        .subscribe((newappointment) => {
-        this.router.navigate(['./admin/appointments']);
-        });
-      }
-    }
-    this.SameAppointmentDate = [];
+    if (this.form.valid) {             
+      // Post to Clients
+      const client = { 
+        'Clients_key' : '',
+        'FullName': this.FullName,
+        'DocumentType': this.identificationtype[0],
+        'ID': this.ID,
+        'Cellphone': this.CellPhone,
+        'Address': this.Address,
+        'email': this.Email,
+      }  
+                     
+      this.clientsService.createClient(client)
+      .subscribe((newClient) => {
+      this.router.navigate(['./admin']);
+      });      
+    }   
   }
-
-  fetchAppointments() {
-    this.appointmentsService.getAllAppointments()
-    .subscribe(appointments => {
-      this.appointments = appointments;
-    });
-  }
-
-  AppointmentSameDate(SameDate: Date, selectedPatientID: number) {
-    this.appointments.forEach(appointment => {
-      const PatientID = Number(appointment.PatientId);
-      if (PatientID === selectedPatientID) {
-        this.DateAppointmentPatient = new Date(appointment.Date);
-        this.SameDate = new Date(SameDate);
-        const diffDays: number = (this.SameDate - this.DateAppointmentPatient) / (1000 * 60 * 60 * 24 );
-        if (diffDays > -1 && diffDays < 1) {
-          this.SameAppointmentDate.push('true');
-        }
-      }
-    });
-    return this.SameAppointmentDate;
-  }
+  
 
   private buildForm() {
     this.form = this.formBuilder.group({
-     PatientId: ['', [Validators.required]],
-     appointmentType: ['', [Validators.required]],
-     Date: ['', [Validators.required]]
+     FullName: ['', [Validators.required]],
+     DocumentType: ['', [Validators.required]],
+     ID: ['', [Validators.required]],
+     Cellphone: ['', [Validators.required]],
+     Address: ['', [Validators.required]],
+     email: ['', [Validators.required]],
     });
-  }
-
-  FecthAppointmentsID() {
-    this.appointmentsService.getAllAppointments()
-    .subscribe(appointments => {
-      const ID = appointments.length + 1;
-      this.IdAppointment = ID;
-    });
-  }
-
-  FecthPatients() {
-    this.clientsService.getAllClients()
-    .subscribe(patients => {
-      this.patientsNames = this.findPatientsName(patients);
-    });
-  }
-
-  findPatientsName(patients) {
-    patients.forEach(patient => {
-      this.patientsNames.push(patient.Name);
-      this.patientsIds.push(patient.PatientId);
-    });
-    return this.patientsNames;
-  }
+  } 
+  
 }
